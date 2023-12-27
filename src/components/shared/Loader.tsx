e82b32ac124b159eaea78659e786f7875ebfaa0a
yourusername <email@youremail.com>
2023-12-27 15:27:12 +0300

    first commit

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,10 @@
+const loader = () => {
+    return (
+      <div className="flex-center w-full">
+          <img src="/assets/icons/loader.svg" alt="loader"
+          width={24} height={24} />
+      </div>
+    )
+  }
+  
+  export default loader
